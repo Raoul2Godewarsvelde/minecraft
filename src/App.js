a0baa9221da186e6a4d84f3a8a87277b010fe9f1
MokoMaracucho <desmoulin.federico@gmail.com>
2022-05-21 14:35:06 +0200

    OK: import Header from '@components/Header/Header'

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from '@pages/Home/index'
 import MySmallWorld from '@pages/MySmallWorld/index'
 import Blog from '@pages/Blog/index'
 
-import Header from './Components/Header/Header'
+import Header from '@components/Header/Header'
 
 import './App.scss'
 
