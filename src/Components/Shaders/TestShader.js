072c41d09d838ec3d4ff6b108ca181e504d61642
MokoMaracucho <desmoulin.federico@gmail.com>
2022-05-21 12:11:07 +0200

    OK: apply texture

--- src/Components/Shaders/TestShader.js
+++ src/Components/Shaders/TestShader.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 
 import * as THREE from 'three'
+import { useLoader } from '@react-three/fiber'
+
+/* import ListImages from '../../Json/ListImages' */
+
+import raoul from '../../Assets/Textures/raoul_profil_1.png'
 
 import testMaterial from '../../scene/material/TestMaterial'
 
 const TestShader = () => {
+    
+  /* const images = useLoader(
+      THREE.TextureLoader,
+      ListImages.map(({ image }) => image)
+  )
+  useMemo(() => images.forEach((texture) => (texture.minFilter = THREE.LinearFilter)), [images])
+  console.log('images', images) */
+    
+  const image = useLoader(
+      THREE.TextureLoader,
+      raoul
+  )
+  console.log('image', image)
 
   return (
     <mesh
@@ -21,6 +39,8 @@ const TestShader = () => {
         <testMaterial
             attach='material'
             side={THREE.DoubleSide}
+            /* uTexture={images[0]} */
+            uTexture={image}
             transparent
         />
     </mesh>
--- src/scene/material/TestMaterial.js
+++ src/scene/material/TestMaterial.js
@@ -6,19 +6,23 @@ import glsl from 'babel-plugin-glsl/macro'
 
 const TestMaterial = shaderMaterial(
   {
-    uResolution: new THREE.Vector2()
+    uResolution: new THREE.Vector2(),
+    uTexture: new THREE.Texture()
   },
   glsl`
     varying vec2 vUv;
     void main() {
+      vUv = uv;
       vec3 pos = position;
       gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
     }
   `,
   glsl`
     varying vec2 vUv;
+    uniform sampler2D uTexture;
     void main() {
-      gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+      vec4 texture = texture2D(uTexture, vUv);
+      gl_FragColor = texture;
     }
   `
 )
