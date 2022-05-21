286752876359a76a8e6e360c73eb2fd4d3dd876a
MokoMaracucho <desmoulin.federico@gmail.com>
2022-05-21 21:11:00 +0200

    OK: cube de base

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,12 +1,32 @@
-import React from 'react'
+import React, { createRef } from 'react'
 
+import { Sky } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
+
+/* import { PerspectiveCamera } from '@components/Cameras/Registration/index' */
+import { OrbitControls, PerspectiveCamera } from '@react-three/drei'
+
+import { Cube } from '@components/Meshes/Primitives/Registration/index'
 
 const Home = () => {
 
+    const camera = createRef()
+
     return (
-        <Canvas>
-            <color attach='background' args={'red'} />
+        <Canvas id={'home_scene'}>
+            {/* <color attach='background' args={'#000'} /> */}
+            <ambientLight />
+            <PerspectiveCamera position={[20, 20, 20]} makeDefault />
+            <OrbitControls />
+            {/* <PerspectiveCamera
+                ref={camera}
+                canvasID={'home_scene'}
+                position={[10, 10, 10]}
+            /> */}
+            <Cube
+                position={[0, 0, 0]}
+                size={[1, 1, 1]}
+            />
         </Canvas>
     )
 }
